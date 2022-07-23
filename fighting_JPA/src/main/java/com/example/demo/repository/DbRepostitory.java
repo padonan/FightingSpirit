0e3d8b2bf004797ba4d3457e5bd1d6691064cffd
Justin lee <85321903+jusenglee@users.noreply.github.com>
2022-07-23 23:43:34 +0900

    Delete fighting_JPA directory

--- fighting_JPA/src/main/java/com/example/demo/repository/DbRepostitory.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.DbEntiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DbRepostitory extends JpaRepository<DbEntiry, Long> {
-
-}
